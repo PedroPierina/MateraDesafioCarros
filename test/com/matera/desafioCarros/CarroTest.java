41f6690b0966269badf8d039c3830c23fb16d069
PedroPierina <PedroPierina@users.noreply.github.com>
2019-04-26 16:08:00 -0300

    Add files via upload

--- test/com/matera/desafioCarros/CarroTest.java
+++ test/com/matera/desafioCarros/CarroTest.java
@@ -1,5 +1,6 @@
 package com.matera.desafioCarros;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,9 +12,9 @@ public class CarroTest {
 	
 	@Test
 	public void testValorLiquido(){
-		Peca volante = new Peca("Volante", "Hyundai", 200, Cidades.CAMPINAS);
-		Peca motor = new Peca("Motor", "Hyundai", 5000, Cidades.SUMARE);
-		Peca cambio = new Peca("Cambio", "Hyundai", 3000, Cidades.SAO_PAULO);
+		Peca volante = new Peca("Volante", "Hyundai", new BigDecimal(200.00), Cidades.CAMPINAS);
+		Peca motor = new Peca("Motor", "Hyundai", new BigDecimal(5000), Cidades.SUMARE);
+		Peca cambio = new Peca("Cambio", "Hyundai", new BigDecimal(3000), Cidades.SAO_PAULO);
 		
 		List<Peca> pecas = new ArrayList<Peca>();
 		
@@ -23,25 +24,25 @@ public class CarroTest {
 		
 		Carro hb20 = new Carro("hb20", "hyundai", pecas, Cidades.CAMPINAS);
 		
-		Assert.assertEquals(13988, hb20.getValorLiquido());
+		Assert.assertEquals(new BigDecimal("13988.0000"), hb20.getValorLiquido());
 	}
 	
 	@Test
 	public void testValorVolante(){
-		Peca volante = new Peca("Volante", "Hyundai", 200, Cidades.CAMPINAS);
-		Assert.assertEquals(260, volante.getValorLiquido());
+		Peca volante = new Peca("Volante", "Hyundai", new BigDecimal(200.00), Cidades.CAMPINAS);
+		Assert.assertEquals(new BigDecimal("260.00"), volante.getValorLiquido());
 	}
 	
 	@Test
 	public void testValorMotor(){
-		Peca motor = new Peca("Motor", "Hyundai", 5000, Cidades.SUMARE);
-		Assert.assertEquals(6000, motor.getValorLiquido());
+		Peca motor = new Peca("Motor", "Hyundai", new BigDecimal(5000), Cidades.SUMARE);
+		Assert.assertEquals(new BigDecimal("6000.00"), motor.getValorLiquido());
 	}
 	
 	@Test
 	public void testValorCambio(){
-		Peca cambio = new Peca("Cambio", "Hyundai", 3000, Cidades.SAO_PAULO);
-		Assert.assertEquals(4500, cambio.getValorLiquido());
+		Peca cambio = new Peca("Cambio", "Hyundai", new BigDecimal(3000), Cidades.SAO_PAULO);
+		Assert.assertEquals(new BigDecimal("4500.00"), cambio.getValorLiquido());
 	}
 
 }
